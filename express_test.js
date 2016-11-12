5a225bd27d727a060f3684bd46432ce3ee38aaa7
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-11-12 15:27:28 +0000

    Now added example of JSON being served as a response

--- express_test.js
+++ express_test.js
@@ -11,3 +11,16 @@ app.get("/", function(request, response){
 
 	response.send("HELLO :D :D :D ");
 });
+
+app.get("/json-test", function(request, response){
+	response.setHeader('Content-Type', 'application/json');
+	
+	var json = {
+		responses : {
+			responeText : "responseText",
+			responseValue : 40
+		}
+	};
+
+	response.send(json);
+});
