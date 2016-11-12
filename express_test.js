e23eec44e2d584c823685828965739c4519c1fea
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2016-11-12 14:54:02 +0000

    Initial commit, express test working

---
+++ express_test.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+app.listen(3000 , function() {
+    console.log("running");
+});
+
+
+app.get("/", function(request, response){
+
+	response.send("HELLO :D :D :D ");
+});
